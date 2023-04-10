846bb85a257747c93e3fd627a718a4a473366c6b
singingw <op0408io1998@gmail.com>
2023-04-10 20:47:01 +0800

    連線 MySQL 並建立 Users 資料表

---
+++ models/user.js
@@ -0,0 +1,21 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: true,
+  })
+  return User
+}
