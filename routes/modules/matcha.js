5ec99eddcd3baa81c18fe6d0850baf458d6de6c8
singingw <op0408io1998@gmail.com>
2023-04-12 01:55:00 +0800

    新增 category 種子資料與修改其相關路由

--- routes/modules/matcha.js
+++ routes/modules/matcha.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { Matcha } = require('../../models')
+const { Matcha,Category } = require('../../models')
 
 router.get('/', (req, res) => {
   res.render('index')
@@ -8,22 +8,26 @@ router.get('/', (req, res) => {
 router.get('/create', (req, res) => {
   res.render('create')
 })
-router.post('/create', (req, res) => {
-  const { name, tel, address, openingHours, website, district } = req.body
-  //if (!name) throw new Error('name is required!')
-  Matcha.create({
-    name,
-    tel,
-    address,
-    openingHours,
-    website,
-    district
-  })
-    .then(() => {
-      req.flash('success_messages', 'successfully created')
-      res.redirect('index')
+router.post('/create', async (req, res) => {
+  const { name, tel, address, openingHours, website, district, category } = req.body
+  if (!name) throw new Error('name is required!')
+  try {
+    const existingCategory = await Category.findOne({ 
+      where: { name:category },
+      raw: true
     })
-    .catch(err => console.error(err))
+    await Matcha.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      website,
+      district,
+      categoryId:existingCategory.id
+    })
+    req.flash('success_messages', 'successfully created')
+    res.redirect('index')
+  } catch (error) { console.error(error) }
 })
 
 module.exports = router
---
+++ seeders/20230411170247-categories-seed-file.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Categories',
+      ['下午茶', '工作室', '餐廳', '西點']
+        .map(item => {
+          return {
+            name: item,
+            created_at: new Date(),
+            updated_at: new Date()
+          }
+        }
+        ), {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Categories', {})
+  }
+};
